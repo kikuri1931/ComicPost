5ad627494150a70e7d9341c763dd5e99461612c5
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-10-07 08:21:44 +0000

    [Add] イラスト・マンガ詳細画面追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,11 +8,13 @@ class PicturesController < ApplicationController
   	@picture = Picture.new (picture_params)
   	@picture.user_id = current_user.id
 	  @picture.save
-    redirect_to user_path(current_user)
+    redirect_to picture_path(@picture)
   end
 
   def show
   	@picture = Picture.find(params[:id])
+    @pictures = @picture.user.pictures.limit(3)
+    @picture_images = @picture.picture_images
   end
 
   def edit
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c6a177dfd1f09924f2270645440cedea2e8731236f5685c74d0d683ae82cec38e175d443a2312f61dda2ddf9858d0dffa3e19d2ac044e6c6d167cad83c95bbdc'
