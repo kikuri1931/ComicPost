d806eeb8c20af74be61d0b8644b474292e6959eb
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-10-11 10:32:41 +0000

    [Add] 会員情報編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,4 +32,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :name_kana, :profile_image, :nickname, :postal_code, :address, :telephone_number, :email, :status, :is_deleted)
+  end
 end
