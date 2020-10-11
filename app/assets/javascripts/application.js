a3e684bc87eb1062b93e5ab6a30efdc2f1389393
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-10-11 08:55:46 +0000

    [Add]新規登録タブメニュー追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,22 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+document.addEventListener("turbolinks:load", function(){
+	$('html,body').animate({ scrollTop: 0 }, 0.1);
+	$('#tab-contents .tab[id != "tab1"]').hide();
+
+	$('#tab-menu a').on('click', function(event) {
+  	$("#tab-contents .tab").hide();
+  	$("#tab-menu .active").removeClass("active");
+  	$(this).addClass("active");
+  	$($(this).attr("href")).show();
+  	event.preventDefault();
+	});
+	$('.tab-link2').click(function(event) {
+		$("#tab-forms .tab").hide();
+	});
+	$('.tab-link1').click(function(event) {
+		$("#tab-forms .tab").show();
+	});
+});
