24a2e267a3719baa08575a4ac930bdf613bb798e
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-10-13 12:49:40 +0000

    [Add] 非同期通信実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    picture = Picture.find(params[:picture_id])
+    @picture = Picture.find(params[:picture_id])
     comment = current_user.comments.new(comment_params)
-    comment.picture_id = picture.id
+    comment.picture_id = @picture.id
     comment.save
-    redirect_back(fallback_location: picture_path(picture))
   end
 
   def destroy
-    picture = Picture.find(params[:picture_id])
+    @picture = Picture.find(params[:picture_id])
     Comment.find_by(id: params[:id], picture_id: params[:picture_id]).destroy
-    redirect_back(fallback_location: picture_path(picture))
   end
 
   private
