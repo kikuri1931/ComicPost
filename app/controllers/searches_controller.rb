28cae469ee5d443a49e0f5701bb9c580622cf51f
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-10-25 06:46:33 +0000

    [Fix] ページネーション非表示修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,13 +5,14 @@ class SearchesController < ApplicationController
 		word = params[:word]
 
 		if @range == '1'
-			@users = User.search_user(word).page(params[:page]).per(10)
+			@users = User.search_user(word).page(params[:page]).per(1)
 		elsif @range == '2'
 			@pictures = Picture.search_picture("マンガ", word).page(params[:page]).per(20)
 		elsif @range == '3'
 			@pictures = Picture.search_picture("イラスト", word).page(params[:page]).per(20)
 		else
-			@genres = Genre.where("genre LIKE?","%#{word}%")
+			genres = Genre.where("genre LIKE?","%#{word}%").pluck('id')
+			@genre_pictures = Picture.where(genre_id: genres).page(params[:page]).per(20)
 		end
 	end
 end
