60ceb8cc9ec051d7ad51e76ee28417d3c5b44305
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-10-25 13:51:39 +0000

    [Fix] 本番環境でユーザ検索結果が1件ずつ表示するバグ修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,7 +6,7 @@ class SearchesController < ApplicationController
 		word = params[:word]
 
 		if @range == "1"
-			@users = User.search_user(word).page(params[:page]).per(1)
+			@users = User.search_user(word).page(params[:page]).per(10)
 		elsif @range == "2"
 			@pictures = Picture.search_picture("マンガ", word).page(params[:page]).per(20)
 		elsif @range == "3"
