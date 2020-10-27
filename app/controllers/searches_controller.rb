e20e472695c2a28443a7fa43463226fdf4088fab
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-10-27 06:17:37 +0000

    [Fix] 検索結果レイアウト調整

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,12 +8,12 @@ class SearchesController < ApplicationController
 		if @range == "1"
 			@users = User.search_user(word).page(params[:page]).per(10)
 		elsif @range == "2"
-			@pictures = Picture.search_picture("マンガ", word).page(params[:page]).per(20)
+			@pictures = Picture.search_picture("マンガ", word).page(params[:page]).per(30)
 		elsif @range == "3"
-			@pictures = Picture.search_picture("イラスト", word).page(params[:page]).per(20)
+			@pictures = Picture.search_picture("イラスト", word).page(params[:page]).per(30)
 		else
 			genres = Genre.search_genre(word)
-			@genre_pictures = Picture.where(genre_id: genres).page(params[:page]).per(20)
+			@genre_pictures = Picture.where(genre_id: genres).page(params[:page]).per(30)
 		end
 	end
 end
