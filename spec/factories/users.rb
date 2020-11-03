9aecab329d8936c64f207fe9daef7768825e6f7d
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-11-03 12:22:32 +0000

    [Add] テスト用ユーザデータ作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    name {"山田太郎"}
+    name_kana {"やまだたろう"}
+    nickname {Faker::Lorem.characters(number: 20)}
+    sequence(:email) { |n| "Test#{n}@example.com"}
+    password {"password"}
+    password_confirmation { 'password' }
+  end
+end
