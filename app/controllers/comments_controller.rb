b56f5e301241fc01e087c00709de3ff0283dca46
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-11-23 08:52:04 +0000

    [add] コメント未入力エラー表示追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,12 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   def create
     @picture = Picture.find(params[:picture_id])
-    comment = current_user.comments.new(comment_params)
-    comment.picture_id = @picture.id
-    comment.score = Language.get_data(comment_params[:comment])
-    comment.save
+    @comment = current_user.comments.new(comment_params)
+    @comment.picture_id = @picture.id
+    @comment.score = Language.get_data(comment_params[:comment])
+    unless @comment.save
+      render 'error'
+    end
   end
 
   def destroy
