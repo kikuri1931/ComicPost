2ec5690fb54d5706d0bab60a04711fd7bc0a09b1
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-11-25 12:37:32 +0000

    [Fix] UserダミーデータUser systemテスト通すため修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,11 @@
 FactoryBot.define do
   factory :user do
-    name {"山田太郎"}
-    name_kana {"やまだたろう"}
-    nickname {Faker::Lorem.characters(number: 20)}
-    sequence(:email) { |n| "Test#{n}@example.com"}
+    name {Faker::Internet.username(specifier: 5)}
+    name_kana {Faker::Internet.username(specifier: 5)}
+    postal_code {'000-0000'}
+    address {Faker::Lorem.characters(number: 20)}
+    telephone_number {'000-0000-0000'}
+    sequence(:email) {Faker::Internet.email}
     password {"password"}
     password_confirmation { 'password' }
     status { :有料会員 }
