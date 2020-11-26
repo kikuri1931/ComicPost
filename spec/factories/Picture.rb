a95e2b1966a1689bf846f838384e297ab0609e5c
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-11-26 12:12:36 +0000

    [Fix] Pictureダミーデータ修正

--- spec/factories/Picture.rb
+++ spec/factories/Picture.rb
@@ -2,7 +2,13 @@ FactoryBot.define do
   factory :picture do
     title { Faker::Lorem.characters(number:5) }
     introduction { Faker::Lorem.characters(number:20) }
-    status { :マンガ }
-    user
+
+    trait :comic do
+      status { :マンガ }
+    end
+    
+    trait :illustration do
+      status { :イラスト }
+    end
   end
 end
