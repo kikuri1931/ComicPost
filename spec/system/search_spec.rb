e31a7b733e9f664b3c71b3ca8a11a3367063cbfa
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-11-30 12:25:18 +0000

    [Add] 検索窓テスト追加

--- spec/system/search_spec.rb
+++ spec/system/search_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe '検索機能のテスト' do
   let!(:user) { create(:user, :paid) }
-  let!(:use2) { create(:user, :paid, nickname: Faker::Lorem.characters(number: 20)) }
+  let!(:user2) { create(:user, :paid, nickname: Faker::Lorem.characters(number: 20)) }
   let!(:deleted_user) { create(:user, :paid, is_deleted: true) }
   let!(:genre) { create(:genre) }
   let!(:deleted_genre) { create(:genre, is_active: false) }
@@ -22,23 +22,118 @@ describe '検索機能のテスト' do
     context '表示の確認' do
       it '名前が表示される' do
         fill_in 'word', with: user.name
-        choose 'ユーザー'
+        select 'ユーザー'
         click_button '検索'
         expect(page).to have_content user.name
         expect(page).to have_content user.status
       end
       it 'ニックネームが表示される' do
         fill_in 'word', with: user2.nickname
-        choose 'ユーザー'
+        select 'ユーザー'
         click_button '検索'
         expect(page).to have_content user2.nickname
         expect(page).to have_content user2.status
       end
-      it '該当なしが表示される' do
-        fill_in 'word', with: 'ああ'
-        choose 'ユーザー'
+      it '退会ユーザが表示されない' do
+        fill_in 'word', with: deleted_user.name
+        select 'ユーザー'
         click_button '検索'
         expect(page).to have_content '該当するユーザはいません'
+      end
+      it '該当なしが表示される' do
+        fill_in 'word', with: 'ああ'
+        select 'ユーザー'
+        click_button '検索'
+        expect(page).to have_content '該当するユーザはいません'
+      end
+    end
+  end
+
+  describe 'マンガ検索のテスト' do
+    context '表示の確認' do
+      it 'マンガが表示される' do
+        fill_in 'word', with: comic.title
+        select 'マンガ'
+        click_button '検索'
+        expect(page).to have_content comic.title
+        expect(page).to have_content comic.user.name
+      end
+      it '退会ユーザ投稿マンガが表示されない' do
+        fill_in 'word', with: deleted_user_comic.title
+        select 'マンガ'
+        click_button '検索'
+        expect(page).to have_content '該当する作品はありせん'
+      end
+      it 'ジャンル無効マンガが表示されない' do
+        fill_in 'word', with: deleted_comic.title
+        select 'マンガ'
+        click_button '検索'
+        expect(page).to have_content '該当する作品はありせん'
+      end
+      it '該当なしが表示される' do
+        fill_in 'word', with: 'ああ'
+        select 'マンガ'
+        click_button '検索'
+        expect(page).to have_content '該当する作品はありせん'
+      end
+    end
+  end
+
+  describe 'イラスト検索のテスト' do
+    context '表示の確認' do
+      it 'イラストが表示される' do
+        fill_in 'word', with: illustration.title
+        select 'イラスト'
+        click_button '検索'
+        expect(page).to have_content illustration.title
+        expect(page).to have_content illustration.user.name
+      end
+      it '退会ユーザ投稿イラストが表示されない' do
+        fill_in 'word', with: deleted_user_illustration.title
+        select 'イラスト'
+        click_button '検索'
+        expect(page).to have_content '該当する作品はありせん'
+      end
+      it 'ジャンル無効イラストが表示されない' do
+        fill_in 'word', with: deleted_illustration.title
+        select 'イラスト'
+        click_button '検索'
+        expect(page).to have_content '該当する作品はありせん'
+      end
+      it '該当なしが表示される' do
+        fill_in 'word', with: 'ああ'
+        select 'イラスト'
+        click_button '検索'
+        expect(page).to have_content '該当する作品はありせん'
+      end
+    end
+  end
+
+  describe 'ジャンル検索のテスト' do
+    context '表示の確認' do
+      it '退会ユーザ投稿作品が表示されない' do
+        fill_in 'word', with: genre
+        select 'ジャンル'
+        click_button '検索'
+        expect(page).to have_no_content deleted_user_comic.title
+        expect(page).to have_no_content deleted_user_comic.user.name
+        expect(page).to have_no_content deleted_user_illustration.title
+        expect(page).to have_no_content deleted_user_illustration.user.name
+      end
+      it 'ジャンル無効作品が表示されない' do
+        fill_in 'word', with: genre
+        select 'ジャンル'
+        click_button '検索'
+        expect(page).to have_no_content deleted_comic.title
+        expect(page).to have_no_content deleted_comic.user.name
+        expect(page).to have_no_content deleted_illustration.title
+        expect(page).to have_no_content deleted_illustration.user.name
+      end
+      it '該当なしが表示される' do
+        fill_in 'word', with: 'ああ'
+        select 'ジャンル'
+        click_button '検索'
+        expect(page).to have_content '該当するジャンル、または作品はありせん'
       end
     end
   end
