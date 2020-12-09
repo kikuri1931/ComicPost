3b387b629630dd065807950a423e01eb4656576f
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-12-09 10:22:24 +0000

    [Fix] ジャンル登録バリデーション追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,6 +2,7 @@ class Genre < ApplicationRecord
   has_many :pictures, dependent: :destroy
 
   validates :genre, presence: true
+  validates :genre, length: {maximum: 15}
   validates :is_active, inclusion: { in: [true, false] }
 
   # ジャンル検索で、作品を探すロジック
